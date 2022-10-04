fab95912631f31adabf36e53c4602dad52bd8182
kishankunvardia <kishankunvardia@hotmail.co.uk>
2022-10-04 13:07:33 +0100

    QC-3 updated link names

--- react/qa-cinema/src/components/Navbar.jsx
+++ react/qa-cinema/src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
 
@@ -7,31 +7,36 @@ const Navbar = () => {
         <header>
             <nav class="navbar navbar-expand-lg navbar-light bg-light">
                 <div class="container-fluid">
-                    <a class="navbar-brand" href="#">Navbar</a>
+                    <Link class="navbar-brand" to="#">Navbar</Link>
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
                     </button>
                     <div class="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                             <li class="nav-item">
-                                <a class="nav-link active" aria-current="page" href="#">Home</a>
+                                <Link class="nav-link" to="#">What's On</Link>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link" href="#">Link</a>
+                                <Link class="nav-link" to="#">Upcoming Films</Link>
+                            </li>
+                            <li class="nav-item">
+                                <Link class="nav-link" to="#">Booking</Link>
+                            </li>
+                            <li class="nav-item">
+                                <Link class="nav-link" to="#">Discussion Board</Link>
                             </li>
                             <li class="nav-item dropdown">
-                                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                    Dropdown
-                                </a>
+                                <Link class="nav-link dropdown-toggle" to="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                    Cinema Info
+                                </Link>
                                 <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                                    <li><a class="dropdown-item" href="#">Action</a></li>
-                                    <li><a class="dropdown-item" href="#">Another action</a></li>
-                                    <li><hr class="dropdown-divider" /></li>
-                                    <li><a class="dropdown-item" href="#">Something else here</a></li>
+                                    <li><Link class="dropdown-item" to="#">Opening Times</Link></li>
+                                    <li><Link class="dropdown-item" to="#">Classifications</Link></li>
+                                    <li><Link class="dropdown-item" to="#">Places to Go</Link></li>
+                                    <li><Link class="dropdown-item" to="#">Find Us</Link></li>
+                                    <li><Link class="dropdown-item" to="#">Contact Us</Link></li>
+                                    <li><Link class="dropdown-item" to="#">About Us</Link></li>
                                 </ul>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
                             </li>
                         </ul>
                         <form class="d-flex">
