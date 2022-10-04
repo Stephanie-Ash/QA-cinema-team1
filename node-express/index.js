4398ed50bada4372b46867fc534b12ba55883aa1
Stephanie-Ash <stephanie.ashdown@hotmail.co.uk>
2022-10-04 15:35:03 +0100

    Add .env file and database connection to express app

--- node-express/index.js
+++ node-express/index.js
@@ -1,27 +1,28 @@
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+dotenv.config();
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
-// Decide on database name for connection!
-// mongoose.connect("mongodb://localhost:27017/", {useNewUrlParser : true}, 
-// (error) => {
-//     if(error) {
-//         console.log("No database connection could be made.");
-//     } else if(!error) {
-//         console.log("Connected to the database.");
-//     }
-// })
+mongoose.connect(process.env.MONGO_URL, {useNewUrlParser : true}, 
+(error) => {
+    if(error) {
+        console.log("No database connection could be made.");
+    } else if(!error) {
+        console.log("Connected to the database.");
+    }
+})
 
 const server = app.listen(3001, () => {
     console.log(`Server started successfully on port: ${server.address().port}`);
 })
 
-// server.on("close", () => {
-//     mongoose.connection.close();
-// })
+server.on("close", () => {
+    mongoose.connection.close();
+})
 
 module.exports = server;
