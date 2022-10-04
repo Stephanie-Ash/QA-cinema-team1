62bedc78bcc3b87ac87c76e371a371b0b73de4d0
Rebecca Swinton <rebecca.swinton@hotmail.com>
2022-10-04 16:16:36 +0100

    QC-1 openingtimes started

--- react/qa-cinema/src/App.js
+++ react/qa-cinema/src/App.js
@@ -14,6 +14,7 @@ function App() {
       <Navbar />
       <Routes>
         <Route>
+          
 
         </Route>
       </Routes>
---
+++ react/qa-cinema/src/components/Openingtimes/OpeningTimes.jsx
@@ -0,0 +1,17 @@
+
+const OpeningTimes = () => {
+    return (
+        <>
+        <h1>QA Cinema</h1>
+        <p>Monday:  07:00 - 01:00</p>
+        <p>Tuesday: 07:00 - 01:00</p>
+        <p>Wednesday: 07:00 - 01:00</p>
+        <p>Thursday: 07:00 - 01:00</p>
+        <p>Friday: 07:00 - 01:00</p>
+        <p>Saturday: 08:30 - 01:00</p>
+        <p>Sunday: 11:00 - 01:00</p>
+        </>
+)
+}
+
+export default OpeningTimes;
