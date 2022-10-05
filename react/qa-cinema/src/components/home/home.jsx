62fc4a47619c7cbc5ccf267f90bc344fe9639302
Stephanie-Ash <stephanie.ashdown@hotmail.co.uk>
2022-10-05 12:12:14 +0100

    QC-10 Add info section to homepage

--- react/qa-cinema/src/components/home/home.jsx
+++ react/qa-cinema/src/components/home/home.jsx
@@ -1,11 +1,13 @@
 import './home.css';
 import Carousel from "./Carousel.jsx";
+import Info from "./Info.jsx";
 
 const Home = () => {
 
     return (
         <>
             <Carousel/>
+            <Info/>
         </>
     )
 }
