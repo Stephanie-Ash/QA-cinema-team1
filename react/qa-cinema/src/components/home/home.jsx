0cf4b2b6d8bde177c1678e3a4f31c6de615bf4c3
Stephanie-Ash <stephanie.ashdown@hotmail.co.uk>
2022-10-05 12:15:16 +0100

    QC-10 Add Link cards to homepage

--- react/qa-cinema/src/components/home/home.jsx
+++ react/qa-cinema/src/components/home/home.jsx
@@ -1,6 +1,11 @@
 import './home.css';
 import Carousel from "./Carousel.jsx";
 import Info from "./Info.jsx";
+import LinkCard from './LinkCard';
+
+import whatsOn from '../../images/home-whats-on.jpg';
+import comingSoon from '../../images/home-coming-soon.jpg';
+import thingsToDo from '../../images/things-to-do.jpg';
 
 const Home = () => {
 
@@ -8,6 +13,13 @@ const Home = () => {
         <>
             <Carousel/>
             <Info/>
+            <section className="container-fluid mb-5">
+                <div className="row">
+                    <LinkCard image={whatsOn} page={"Link to Whats On page"}/>
+                    <LinkCard image={comingSoon} page={"Link to Coming Soon page"}/>
+                    <LinkCard image={thingsToDo} page={"Link to Things To Do page"}/>
+                </div>
+            </section>
         </>
     )
 }
