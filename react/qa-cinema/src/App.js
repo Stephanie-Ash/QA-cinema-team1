346aac30310f3758b3537222cf2da15d9a7dc919
Leo Gornovskiy <lgornovskiy@protonmail.com>
2022-10-05 12:58:02 +0100

    testing

--- react/qa-cinema/src/App.js
+++ react/qa-cinema/src/App.js
@@ -1,10 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import * as mdb from 'mdb-ui-kit'; // lib
+import { Input } from 'mdb-ui-kit'; // module
 import {
   BrowserRouter, Routes, Route
 } from "react-router-dom";
 
 import Navbar from './components/Navbar.jsx';
+import Footer from './components/Footer.jsx';
 
 function App() {
   return (
@@ -17,6 +20,7 @@ function App() {
 
         </Route>
       </Routes>
+      <Footer />
 
     </BrowserRouter>
 
