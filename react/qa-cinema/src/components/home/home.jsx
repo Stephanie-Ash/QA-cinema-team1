8fc35e2ca9442ccf68914fd9a5ea47342e9f4174
Stephanie-Ash <stephanie.ashdown@hotmail.co.uk>
2022-10-06 10:39:53 +0100

    QC-10 #done Add film database call to display homepage carousel images

--- react/qa-cinema/src/components/home/home.jsx
+++ react/qa-cinema/src/components/home/home.jsx
@@ -1,3 +1,6 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
 import './home.css';
 import Carousel from "./Carousel.jsx";
 import Info from "./Info.jsx";
@@ -8,10 +11,27 @@ import comingSoon from '../../images/home-coming-soon.jpg';
 import thingsToDo from '../../images/home-things-to-do.jpg';
 
 const Home = () => {
+    const [error, setError] = useState(null);
+    const [isLoaded, setIsLoaded] = useState(false);
+    const [films, setFilms] = useState([]);
 
-    return (
-        <>
-            <Carousel/>
+    useEffect(() => {
+        axios
+        .get("http://localhost:3001/films/current")
+        .then(res => res)
+        .then((result) => {
+            setIsLoaded(true);
+            setFilms(result.data);
+        }, (error) => {
+            setIsLoaded(true);
+            setError(error);
+        });
+    }, []);
+
+    if(error) {
+        return(
+            <>
+            <div>Error loading images: {error.message}</div>
             <Info/>
             <section className="container-fluid mb-5">
                 <div className="row">
@@ -21,7 +41,37 @@ const Home = () => {
                 </div>
             </section>
         </>
-    )
+        )
+    }
+    else if(!isLoaded) {
+        return(
+        <>
+            <div>Loading images...</div>
+            <Info/>
+            <section className="container-fluid mb-5">
+                <div className="row">
+                    <LinkCard image={whatsOn} page={"Link to Whats On page"}/>
+                    <LinkCard image={comingSoon} page={"Link to Coming Soon page"}/>
+                    <LinkCard image={thingsToDo} page={"Link to Things To Do page"}/>
+                </div>
+            </section>
+        </>
+        )
+    } else {
+        return (
+            <>
+                <Carousel movies={films}/>
+                <Info/>
+                <section className="container-fluid mb-5">
+                    <div className="row">
+                        <LinkCard image={whatsOn} page={"Link to Whats On page"}/>
+                        <LinkCard image={comingSoon} page={"Link to Coming Soon page"}/>
+                        <LinkCard image={thingsToDo} page={"Link to Things To Do page"}/>
+                    </div>
+                </section>
+            </>
+        )
+    }  
 }
 
 export default Home;
