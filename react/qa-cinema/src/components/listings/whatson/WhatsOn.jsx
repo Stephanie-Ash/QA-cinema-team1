15ce3cabecc4e0ea0d22ed5ba806d07d85492010
Stephanie-Ash <stephanie.ashdown@hotmail.co.uk>
2022-10-06 17:08:06 +0100

    QC-4 Add film cards to whats on page

--- react/qa-cinema/src/components/listings/whatson/WhatsOn.jsx
+++ react/qa-cinema/src/components/listings/whatson/WhatsOn.jsx
@@ -3,6 +3,7 @@ import axios from 'axios';
 
 import '../listings.css';
 import ListingHeader from '../ListingHeader';
+import FilmCard from '../FilmCard';
 
 const WhatsOn = () => {
     const [error, setError] = useState(null);
@@ -11,37 +12,47 @@ const WhatsOn = () => {
 
     useEffect(() => {
         axios
-        .get("http://localhost:3001/films/current")
-        .then(res => res)
-        .then((result) => {
-            setIsLoaded(true);
-            setFilms(result.data);
-        }, (error) => {
-            setIsLoaded(true);
-            setError(error);
-        });
+            .get("http://localhost:3001/films/current")
+            .then(res => res)
+            .then((result) => {
+                setIsLoaded(true);
+                setFilms(result.data);
+            }, (error) => {
+                setIsLoaded(true);
+                setError(error);
+            });
     }, []);
 
-    if(error) {
-        return(
+    if (error) {
+        return (
             <>
                 <div>Error loading images: {error.message}</div>
+                <FilmCard />
             </>
         )
     }
-    else if(!isLoaded) {
-        return(
+    else if (!isLoaded) {
+        return (
             <>
                 <div>Loading image...</div>
+                <FilmCard />
             </>
         )
     } else {
         return (
             <>
-              <ListingHeader movies={films}/>  
+                <ListingHeader movies={films} />
+                <section className="container-fluid">
+                    <h1>Whats On</h1>
+                    <div className="row mb-5">
+                        {films.map((film) => (
+                            <FilmCard key={film.film_id} image={film.image_url} rating={film.classification} length={film.length} title={film.title} />
+                        ))}
+                    </div>
+                </section>
             </>
         )
-    }  
+    }
 }
 
 export default WhatsOn;
