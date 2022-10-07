06b6b386126c926cec3093cb5a8d7414babd3164
Rebecca Swinton <rebecca.swinton@hotmail.com>
2022-10-07 17:29:48 +0100

    filmsinfopage

--- react/qa-cinema/src/components/PlacesToGo/PlacesInfo.jsx
+++ react/qa-cinema/src/components/PlacesToGo/PlacesInfo.jsx
@@ -19,7 +19,7 @@ const PlacesToGo = () =>
          <img src={Arcade} className="arcade rounded mx-auto d-block" alt="arcade"/>
          <p className ='description'>Looking for something fun? Arcade Art is the talk of the city! Equipped with pinball machines, air hockey tables and the latest arcade videos games such as: Need for Speed 7, Warfare Madness and Duty Chaos. A visit here is a guaranteed good time! Located in Tower Bridge, minutes away from QA Cinema, Indigo's Kitchen and Gravifun.
          <ReadMoreReadLess descriptionLength={70}>
-         <i class="fa-regular fa-circle-info"></i>
+         <i className="fa-regular fa-circle-info"></i>
          <p>Offers: Win 70 tokens to enjoy 10% off your next visit to QA Cinema.
          Win 150 tokens to enjoy 25% off your next visit to QA Cinema</p>
          <p>Address: 8 St Katharine's Way, London E1W 1UN</p>
@@ -34,7 +34,7 @@ const PlacesToGo = () =>
          <img src={Restaurant} className="restaurant rounded mx-auto d-block" alt="restaurant"/>
          <p className ='description'>Located in the heart of Tower Bridge, Indigo's Kitchen is a restaurant and bar with a spacious garden overlooking the Thames. Minutes away from London Bridge and Tower Hill, Indigo's Kitchen is the best spot for enjoying a lovely family meal, dinner for two or group outing. A menu with gluten-free, vegan and vegetarian options, makes us the best place for breakfast, lunch or dinner! A family friendly restaurant that emcompasses soul and vivacity not only in the atmosphere but also in the Kitchen (no pun intended!)  
          <ReadMoreReadLess descriptionLength={70}>
-         <i class="fa-regular fa-circle-info"></i>
+         <i className="fa-regular fa-circle-info"></i>
          <p>Offers: Enjoy 15% of your cinema tickets at QA Cinema when you dine at Indigo's Kitchen between 12pm-4pm
           Enjoy 20% your cinema tickets at QA Cinema when you dine at Indigo's Kitchen between 4pm-8pm</p>
           <p>Address: 12 St Katharine's Way, London E1W 1UN</p>
@@ -48,7 +48,7 @@ const PlacesToGo = () =>
           <img src={Fair} className="fair rounded mx-auto d-block" alt="fair"/>
           <p className='description'>London's greatest Funfair is open for your family outing! Immersive yourself in all things fun, from attractions and entertainment to food and drink! Find us in Tower Bridge, near QA Cinema and Indigo's Kitchen.
           <ReadMoreReadLess descriptionLength={70}>
-          <i class="fa-regular fa-circle-info"></i>
+          <i className="fa-regular fa-circle-info"></i>
          <p>Offers: Buy 3 or more tickets for 15% off nearby attractions such as QA Cinema and Blues Kitchen.</p>
          <p>Address: 17-29 St Katharine's Way, London E1W 1UN</p>
          <p>Contact Details: ParadoxIsland@info.com
--- react/qa-cinema/src/components/listings/films/Film.jsx
+++ react/qa-cinema/src/components/listings/films/Film.jsx
@@ -1,5 +1,7 @@
 import { useLocation } from "react-router-dom";
 import { useOutletContext } from 'react-router-dom';
+import { Link } from "react-router-dom";
+import  "./Film.css";
 
 const Film = () => {
     const location = useLocation();
@@ -7,10 +9,282 @@ const Film = () => {
 
     const [films] = useOutletContext();
     const film = films.filter(film => {
-        return film.film_id === film_id;
+
+        return film.film_id === film_id
+
+
     })
+    console.log(film);
+   
+
+return (
+<>
+<h1>{film[0].title}</h1>
+<div className ='container-fluid'>
+<div className="card-mb-3" style={{maxwidth: '540px'}}>
+  <div className="row g-0">
+    <div className="col-md-4">
+      <img src={film[0].image_url} className="film-img" alt=".."/>
+    </div>
+    <div className="col-md-8">
+      <div className="card-body">
+        <div className = "description-box">
+        <h5 className="card-title">{film[0].title}</h5>
+        <div className="actors">
+            
+
+        <p className="card-actors">Actors: {film[0].actors}</p>
+        </div>
+
+        <p className="card-director">Director(s): {film[0].director}</p>
+        <p className="card-synopsis">Synopsis: {film[0].synopsis}</p>
+        <p className="card-genre">Genre: {film[0].genre}</p>
+        <p className="card-classification">Classification: {film[0].classification}</p>
+        <p className="card-length"><small className="text-muted">Length: {film[0].length}</small></p>
+        </div>
+      </div>
+    </div>
+  </div>
+  </div>
+  <hr></hr>
+  <div className ='times-and-dates'>
+  <div className="container">
+   
+  <div className="row"> 
+  <h3>Today</h3>
+  <p className="deluxe">Deluxe</p>
+    <div className="col">
+        <Link to="#">
+    <button type="button" className="btn btn-outline-danger btn-lg">11:00 AM
+    <p> 2D</p>
+    <p> 10.99</p></button>
+    </Link>
+    </div>
+    <div className="col">
+    <Link to="#">
+    <button type="button" className="btn btn-outline-danger btn-lg">14:30 PM
+    <p> 3D</p>
+    <p> 10.99</p></button>
+    </Link>
+    </div>
+  </div>
+  <div className="row-1">
+    <div className="col">
+ <p className="standard">Standard</p>
+ <Link to="#">
+    <button type="button" class="btn btn-outline-info">11:00 AM
+    <p> 2D</p>
+    <p> 6.99</p></button>
+    </Link>
+    </div>
+    <div className="col">
+    <Link to="#">
+    <button type="button" class="btn btn-outline-info">14:30 PM
+    <p> 2D</p>
+    <p> 6.99</p>
+    </button>
+    </Link>
+    </div>
+    <div className="col">
+    <Link to="#">
+    <button type="button" class="btn btn-outline-info">17:45 PM
+    <p> 2D</p>
+    <p>6.99</p></button>
+    </Link>
+    </div>
+  </div>
+</div>
+<div className="container">
+   
+  <div className="row"> 
+  <h3>Tomorrow</h3>
+  <p className="deluxe">Deluxe</p>
+    <div className="col">
+    <Link to="#">
+    <button type="button" class="btn btn-outline-danger btn-lg">11:00 AM
+    <p> 2D</p>
+    <p> 10.99</p></button>
+    </Link>
+    </div>
+    <div className="col">
+    <Link to="#">
+    <button type="button" class="btn btn-outline-danger btn-lg">14:30 PM
+    <p> 3D</p>
+    <p> 10.99</p></button>
+    </Link>
+    </div>
+  </div>
+  <div className="row-4">
+    <div className="col">
+        <p className="standard">Standard</p>
+    <Link to="#">
+    <button type="button" class="btn btn-outline-info">11:00 AM
+    <p> 2D</p>
+    <p> 6.99</p></button>
+    </Link>
+    </div>
+    <div className="col">
+    <Link to="#">
+    <button type="button" className="btn btn-outline-info">14:30 PM
+    <p> 2D</p>
+    <p> 6.99</p></button>
+    </Link>
+    </div>
+    <div className="col">
+    <Link to="#">
+    <button type="button" className="btn btn-outline-info">17:45 PM
+    <p> 2D</p>
+    <p> 6.99</p></button>
+    </Link>
+    </div>
+  </div>
+</div>
+<div className="container">
+   
+  <div className="row"> 
+  <h3>Wednesday</h3>
+  <p className="deluxe">Deluxe</p>
+    <div className="col">
+    <Link to="#">
+    <button type="button" className="btn btn-outline-danger btn-lg">11:00 AM
+    <p> 2D</p>
+    <p> 10.99</p></button>
+    </Link>
+</div>
+    <div className="col">
+    <Link to="#">
+    <button type="button" className="btn btn-outline-danger btn-lg">14:30 PM
+    <p> 3D</p>
+    <p> 10.99</p></button>
+    </Link>
+      
+    </div>
+  </div>
+  <div className="row-1">
+    <div className="col">
+    <p className="standard">Standard</p>
+    <Link to="#">
+    <button type="button" className="btn btn-outline-info">11:00 AM
+    <p> 2D</p>
+    <p> 6.99</p></button>
+    </Link>
+    </div>
+    <div className="col">
+    <Link to="#">
+    <button type="button" className="btn btn-outline-info">14:30 PM
+    <p> 2D</p>
+    <p> 6.99</p></button>
+    </Link>
+    </div>
+    <div className="col">
+    <Link to="#">
+    <button type="button" className="btn btn-outline-info">17:45 PM
+    <p> 2D</p>
+    <p> 6.99</p>
+    </button>
+    </Link>
+    </div>
+  </div>
+</div>
+<div className="container">
+   
+  <div className="row"> 
+  <h3>Thursday</h3>
+  <p className="deluxe">Deluxe</p>
+    <div className="col">
+    <Link to="#">
+    <button type="button" className="btn btn-outline-danger btn-lg">11:00 AM
+    <p> 2D</p>
+    <p> 10.99</p></button>
+    </Link>
+    </div>
+    <div className="col">
+    <Link to="#">
+    <button type="button" className="btn btn-outline-danger btn-lg">14:30 PM
+    <p> 3D</p>
+    <p> 10.99</p></button>
+      </Link>
+     
+    </div>
+  </div>
+  <div className="row-1">
+    <div className="col">
+         <p className="standard">Standard</p>
+    <Link to="#">
+    <button type="button" className="btn btn-outline-info">11:00 AM
+    <p> 2D</p>
+    <p> 6.99</p></button>
+    </Link>
+    </div>
+    <div className="col">
+    <Link to="#">
+    <button type="button" className="btn btn-outline-info">14:30 PM
+    <p> 2D</p>
+    <p> 6.99</p></button>
+    </Link>
+    </div>
+    <div className="col">
+    <Link to="#">
+    <button type="button" className="btn btn-outline-info">17:45 PM
+    <p> 2D</p>
+    <p> 6.99</p></button>
+    </Link>
+    </div>
+  </div>
+</div>
+<div className="container">
+   
+  <div className="row"> 
+  <h3>Friday</h3>
+  <p className="deluxe">Deluxe</p>
+    <div className="col">
+    <Link to="#">
+    <button type="button" className="btn btn-outline-danger btn-lg">11:00 AM
+    <p> 2D</p>
+    <p> 10.99</p></button>
+    </Link>
+    </div>
+    <div className="col">
+    <Link to="#">
+    <button type="button" className="btn btn-outline-danger btn-lg">14:30 PM
+    <p> 3D</p>
+    <p> 10.99</p></button>
+      </Link>
+      
+    </div>
+  </div>
+  <div className="row-1">
+    <div className="col">
+        <p className="standard">Standard</p>
+    <Link to="#">
+    <button type="button" className="btn btn-outline-info">11:00 AM
+    <p> 2D</p>
+    <p> 6.99</p></button>
+    </Link>
+    </div>
+    <div className="col">
+    <Link to="#">
+    <button type="button" className="btn btn-outline-info">14:30 PM
+    <p> 2D</p>
+    <p> 6.99</p></button>
+   </Link>
+    </div>
+    <div className="col">
+    <Link to="#">
+    <button type="button" className="btn btn-outline-info">17:45 PM
+    <p> 2D</p>
+    <p> 6.99</p></button>
+     </Link>
+
+     
+    </div>
+  </div>
+</div>
+  </div>
+  </div>
+</>
 
 
-}
+)}
 
 export default Film;
