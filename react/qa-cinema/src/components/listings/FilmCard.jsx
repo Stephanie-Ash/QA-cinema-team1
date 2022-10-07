afe198344679c0d4985811e8b090b29ebf0399a5
Stephanie-Ash <stephanie.ashdown@hotmail.co.uk>
2022-10-07 12:18:56 +0100

    QC-5 QC-45 QC-4 #done create upcoming films page and ensure all links functional

--- react/qa-cinema/src/components/listings/FilmCard.jsx
+++ react/qa-cinema/src/components/listings/FilmCard.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 const FilmCard = (props) => {
 
     return (
-        <div className="col-12 col-sm-6 col-md-4 col-lg-3">
+        <div className="col-12 col-sm-6 col-md-4 col-xl-3">
             <Link to={`/listings/film/${props.id}`} state={{film_id: props.id}} className='film-card-link'>
                 <div className="card film-card mx-auto">
                     <img src={props.image} className="card-img-top" alt={props.title} />
--- react/qa-cinema/src/components/listings/ListingHeader.jsx
+++ react/qa-cinema/src/components/listings/ListingHeader.jsx
@@ -1,7 +1,7 @@
 const ListingHeader = (props) => {
     return(
         <section>
-            <img className="listing-head" src={props.movies[0].image_url} alt={props.movies[0].title} />
+            <img className="listing-head" src={props.movies[1].image_url} alt={props.movies[1].title} />
         </section>
     )
 
