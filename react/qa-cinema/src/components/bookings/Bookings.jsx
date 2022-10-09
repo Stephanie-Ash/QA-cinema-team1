c00870693b023a3cb4bcfe7d846e937e643361c9
Stephanie-Ash <stephanie.ashdown@hotmail.co.uk>
2022-10-09 11:13:18 +0100

    Add booking state and initial booking form

--- react/qa-cinema/src/components/bookings/Bookings.jsx
+++ react/qa-cinema/src/components/bookings/Bookings.jsx
@@ -1,4 +1,68 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import { Outlet } from 'react-router-dom';
+
 const Bookings = () => {
+
+    const [error, setError] = useState(null);
+    const [isLoaded, setIsLoaded] = useState(false);
+    const [films, setFilms] = useState([]);
+
+    const [booking, setBooking] = useState(
+        {
+            cust_name: "",
+            cust_email: "",
+            film: {
+                film_id: "",
+                title: ""
+            },
+            date: "",
+            time: "",
+            screen_type: "",
+            adults: 0,
+            children: 0,
+            concessions: 0,
+            total_seats: 0,
+            price: 0,
+            has_paid: false
+        }
+    );
+    
+    useEffect(() => {
+        axios
+            .get("http://localhost:3001/films/current")
+            .then(res => res)
+            .then((result) => {
+                setIsLoaded(true);
+                setFilms(result.data);
+            }, (error) => {
+                setIsLoaded(true);
+                setError(error);
+            });
+    }, []);
+
+    if (error) {
+        return (
+            <div>
+                <p className='mb-0'>Error loading data: {error.message}.</p>
+                <p>Please try again later.</p>
+            </div>
+
+        )
+    }
+    else if (!isLoaded) {
+        return (
+            <div>Loading data...</div>
+        )
+    } else {
+        return (
+            <>
+                <Outlet context={{
+                    films,
+                    booking, setBooking}}/>
+            </>
+        )
+    }
 
 }
 
--- react/qa-cinema/src/components/bookings/makebooking/MakeBooking.jsx
+++ react/qa-cinema/src/components/bookings/makebooking/MakeBooking.jsx
@@ -1,4 +1,73 @@
+import { useOutletContext } from 'react-router-dom';
+
 const MakeBooking = () => {
+
+    const { films } = useOutletContext();
+    const { booking, setBooking } = useOutletContext();
+
+    return (
+        <section className='container-fluid'>
+            <h1>Make a Booking</h1>
+            <div className="row">
+                <div className="col-10 mx-auto">
+                    <form>
+                        <div className="mb-3">
+                            <label htmlFor="film" className="form-label">Film</label>
+                            <select className="form-select" id='film'>
+                                <option>Open this select menu</option>
+                                {films.map((film) => (
+                                    <option key={film.film_id} value={film.title}>{film.title}</option>
+                                ))}
+                            </select>
+                        </div>
+                        <div className="mb-3">
+                            <label htmlFor="screen-type" className="form-label">Screen Type</label>
+                            <select className="form-select" id='screen-type'>
+                                <option>Open this select menu</option>
+                                <option value="standard">Standard</option>
+                                <option value="deluxe">Deluxe</option>
+                            </select>
+                        </div>
+                        <div className="mb-3">
+                            <label htmlFor="date" className="form-label">Date</label>
+                            <select className="form-select" id='date'>
+                                <option>Open this select menu</option>
+                                <option value="standard">Some Date</option>
+                                <option value="deluxe">Another Date</option>
+                            </select>
+                        </div>
+                        <div className="mb-3">
+                            <label htmlFor="time" className="form-label">Time</label>
+                            <select className="form-select" id='time'>
+                                <option>Open this select menu</option>
+                                <option value="standard">Some Time</option>
+                                <option value="deluxe">Another Time</option>
+                            </select>
+                        </div>
+                        <div className="mb-3">
+                            <div className="row">
+                                <div className="col-4">
+                                    <label htmlFor="adults" className="form-label">Adults</label>
+                                    <input type="number" className="form-control" id="adults" />
+                                </div>
+                                <div className="col-4">
+                                    <label htmlFor="children" className="form-label">Children</label>
+                                    <input type="number" className="form-control" id="concessions" />
+                                </div>
+                                <div className="col-4">
+                                    <label htmlFor="concessions" className="form-label">Concessions</label>
+                                    <input type="number" className="form-control" id="concessions" aria-describedby="concessionsHelp"/>
+                                    <div id="concessionsHelp" className="form-text">Students / Seniors</div>
+                                </div>
+                            </div>
+                        </div>
+                        <button type="submit" className="btn btn-primary">Proceed to Payment</button>
+                    </form>
+
+                </div>
+            </div>
+        </section>
+    )
 
 }
 
